96763309e20ce6a0d6afb3ed9c8bf7cd35bbe7d6
Chan1605 <qkqhoe@naver.com>
2022-09-15 18:00:24 +0900

    ㄴㅁㄹ

--- Assets/Scripts/TitleMgr.cs
+++ Assets/Scripts/TitleMgr.cs
@@ -2,23 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class TitleMgr : MonoBehaviour
 {
     public Button ReAnimBtn;
+    public Button PlayBtn;
     public GameObject TitleAnim;
     // Start is called before the first frame update
     void Start()
     {
         if (ReAnimBtn != null)
             ReAnimBtn.onClick.AddListener(AnimStart);
+        if (PlayBtn != null)
+            PlayBtn.onClick.AddListener(GameStart);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GameStart()
+    {
+        SceneManager.LoadScene("InGame");
     }
 
     void AnimStart()
